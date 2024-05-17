5833973f6e10f2a7dcafb15618ca467623fab0d6
mirmehraj <mehraj95he@gmail.com>
2024-05-17 15:10:52 +0530

    Added initial files

---
+++ script.js
@@ -0,0 +1,8 @@
+//changing text of h1.
+// $('h1').text('hello');
+// changing color of h1 tag using JQuery
+// $('h1').css("color","purple");
+// changing color of h1 tag using JQuery
+// $('#btn-1').css("color" , "purple");
+// adding class using JQuery
+// $('h1').addClass("BigFont");
